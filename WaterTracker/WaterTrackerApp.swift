9ad039497ad925db4378070569a196268e1f5f12
Hendrik Minniear Rombach <h.minniear@icloud.com>
2026-08-04 18:40:47 +0200

    chore: scaffold macOS app

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Hendrik Rombach on 04.08.26.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
